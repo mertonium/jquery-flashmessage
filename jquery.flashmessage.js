8f816698c34ee7a96a5619ba07e0f78f06e1bb85
John Mertens <john.mertens@gmail.com>
2011-09-14 19:50:07 -0700

    Initial commit.

---
+++ jquery.flashmessage.js
@@ -0,0 +1,24 @@
+(function($) {
+  $.flashmessage = function(msg, options) {
+    var defaults = {
+      target: 'body',
+      id: 'flashmessage',
+      timeToFade: 3000,
+      type: 'notification'
+    };
+
+    var options = $.extend(defaults, options);
+
+    var $msgBox = $('<div id="'+options.id+'"><div class="flash-wrapper '+options.type+'">'+msg+'</div></div>').css({
+      position: 'fixed',
+      width: '100%',
+    }).hide();
+    $target = (typeof options.target == 'string') ? $(options.target) : options.target;
+    
+    $target.prepend($msgBox);
+    
+    $msgBox.fadeIn().addClass('open-message').delay(options.timeToFade).fadeOut(function(){ $msgBox.remove(); });
+    
+    return true;
+  };
+})(jQuery);
